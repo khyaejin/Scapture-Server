ce6f4343226cf32c7a03b3e0398616b0ea2b7a89
이현승 <77336664+gesal03@users.noreply.github.com>
2024-07-22 01:40:29 +0900

    ✨ Add Schedule Entity

--- scapture/src/main/java/com/server/scapture/domain/Schedule.java
+++ scapture/src/main/java/com/server/scapture/domain/Schedule.java
@@ -1,12 +1,10 @@
 package com.server.scapture.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
-import org.joda.time.DateTime;
+import org.hibernate.annotations.ColumnDefault;
 import org.joda.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Getter
@@ -16,7 +14,13 @@ import org.joda.time.LocalDateTime;
 public class Schedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    private Long id;                // PK
+    @ManyToOne
+    @JoinColumn(name = "field_id")
+    private Field field;            // 구장 FK
+    private LocalDateTime startDate;// 이용 시간(시작)
+    private LocalDateTime endDate;  // 이용 시간(종료)
+    @ColumnDefault("false")
+    private boolean isReserved;     // 예약 여부
+    private int price;              // 가격
 }
