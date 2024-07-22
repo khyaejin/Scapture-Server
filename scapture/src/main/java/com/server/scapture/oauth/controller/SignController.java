66846d248370403d0c8e7095b04d29a53f7599ee
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-07-23 04:57:59 +0900

    ✨ FEAT : Kakao 소셜로그인 - 인가토큰 받기, 접근 토큰 받기, 사용자 정보 받기 구현

--- scapture/src/main/java/com/server/scapture/oauth/controller/SignController.java
+++ scapture/src/main/java/com/server/scapture/oauth/controller/SignController.java
@@ -1,4 +1,48 @@
 package com.server.scapture.oauth.controller;
 
+import com.server.scapture.oauth.service.SignService;
+import com.server.scapture.util.response.CustomAPIResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/oauth")
+@RequiredArgsConstructor
 public class SignController {
+    private static final Logger logger = LoggerFactory.getLogger(SignController.class);
+    private final SignService signService;
+
+    //카카오 소셜 로그인
+    @PostMapping(value = "/social/kakao")
+    public ResponseEntity<CustomAPIResponse<?>> kakaoLogin(@RequestParam String code) {
+        // 1. 인가 코드 받기 (@RequestParam String code)
+
+        // 2. 접근 토큰 받기
+        String accessToken = signService.getAccessToken(code);
+        logger.info("Access Token: {}", accessToken);
+
+        // 3. 사용자 정보 받기
+        Map<String, Object> userInfo = signService.getUserInfo(accessToken);
+
+        String email = (String) userInfo.get("email");
+        String nickname = (String) userInfo.get("nickname");
+
+        logger.info("User Email: {}", email);
+        logger.info("User Nickname: {}", nickname);
+
+        // 필요한 로직 추가
+        // 4. 로그인
+
+        // 5. jwt 토큰 발급
+
+        return null;
+    }
 }
---
+++ scapture/src/main/java/com/server/scapture/oauth/dto/KakaoApi.java
@@ -0,0 +1,16 @@
+/*
+package com.server.scapture.oauth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+@Getter @Setter
+public class KakaoApi {
+    @Value("${KAKAO_CLIENT_ID}") -> 후에 오류 수정해야함
+    private String kakaoApiKey = "024871f91fe647ce7262bd022bd1afc2";
+
+    @Value("${KAKAO_REDIRECT_URI}")
+    private String kakaoRedirectUri = "http://localhost:3000/oauth/redirected/kakao";
+}
+*/
