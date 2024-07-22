2b231d432e4f971ffe5ab7a9c2a8977b1a4d70f4
이현승 <77336664+gesal03@users.noreply.github.com>
2024-07-23 02:05:57 +0900

    ✨ Add HH:mm Logic

--- scapture/src/main/java/com/server/scapture/domain/Schedule.java
+++ scapture/src/main/java/com/server/scapture/domain/Schedule.java
@@ -5,6 +5,8 @@ import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Entity
 @Getter
@@ -23,4 +25,11 @@ public class Schedule {
     @ColumnDefault("false")
     private Boolean isReserved;     // 예약 여부
     private int price;              // 가격
+
+    public String convertHourAndMin() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        String formattedStartDate = startDate.format(formatter);
+        String formattedEndDate = endDate.format(formatter);
+        return formattedStartDate + "~" + formattedEndDate;
+    }
 }
