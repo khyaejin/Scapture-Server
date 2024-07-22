3c77186175ec1370698aa9d742556b0fd91397b2
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-07-22 10:03:06 +0900

    ✨ 소셜로그인 구현 - GoogleUserInfo 생성

---
+++ scapture/src/main/java/com/server/scapture/user/dto/GoogleUserInfo.java
@@ -0,0 +1,24 @@
+package com.server.scapture.user.dto;
+
+import java.util.Map;
+
+public class GoogleUserInfo implements OAuth2UserInfo
+{
+
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("sub");
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+}
