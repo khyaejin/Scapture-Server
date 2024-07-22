f092db3789fe3a890489cf46c7c7ed3b5194b54e
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-07-22 12:02:59 +0900

    🚨 JwtUtill 수정

--- scapture/src/main/java/com/server/scapture/user/JwtUtil.java
+++ scapture/src/main/java/com/server/scapture/user/JwtUtil.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
-import java.util.UUID;
-
 
 @Slf4j
 @Component
@@ -30,20 +28,8 @@ public class JwtUtil {
 
         return Jwts.builder()
                 .claim("userId", userId.toString()) // 클레임에 userId 추가
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expirationMillis))
-                .signWith(this.getSigningKey())
-                .compact();
-    }
-
-    // 리프레쉬 토큰을 발급하는 메서드
-    public String generateRefreshToken(UUID userId, long expirationMillis) {
-        log.info("리프레쉬 토큰이 발행되었습니다.");
-
-        return Jwts.builder()
-                .claim("userId", userId.toString()) // 클레임에 userId 추가
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expirationMillis))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
                 .signWith(this.getSigningKey())
                 .compact();
     }
@@ -56,36 +42,31 @@ public class JwtUtil {
     // 토큰에서 유저 id를 반환하는 메서드
     public String getUserIdFromToken(String token) {
         try {
-            String userId = Jwts.parser()
-                    .verifyWith(this.getSigningKey())
+            return Jwts.parserBuilder()
+                    .setSigningKey(this.getSigningKey())
                     .build()
-                    .parseSignedClaims(token)
-                    .getPayload()
+                    .parseClaimsJws(token)
+                    .getBody()
                     .get("userId", String.class);
-            log.info("유저 id를 반환합니다.");
-            return userId;
         } catch (JwtException | IllegalArgumentException e) {
-            // 토큰이 유효하지 않은 경우
             log.warn("유효하지 않은 토큰입니다.");
-            throw new TokenException(TokenErrorResult.INVALID_TOKEN);
+            return null;
         }
     }
 
     // Jwt 토큰의 유효기간을 확인하는 메서드
-    public boolean isTokenExpired(String token) {
+    public Boolean isTokenExpired(String token) {
         try {
-            Date expirationDate = Jwts.parser()
-                    .verifyWith(this.getSigningKey())
+            Date expirationDate = Jwts.parserBuilder()
+                    .setSigningKey(this.getSigningKey())
                     .build()
-                    .parseSignedClaims(token)
-                    .getPayload()
+                    .parseClaimsJws(token)
+                    .getBody()
                     .getExpiration();
-            log.info("토큰의 유효기간을 확인합니다.");
             return expirationDate.before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
-            // 토큰이 유효하지 않은 경우
             log.warn("유효하지 않은 토큰입니다.");
-            throw new TokenException(TokenErrorResult.INVALID_TOKEN);
+            return null;
         }
     }
 }
