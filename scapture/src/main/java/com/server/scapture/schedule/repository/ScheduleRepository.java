a395090b4c88b07521e761ab7e2926b86c02c259
이현승 <77336664+gesal03@users.noreply.github.com>
2024-07-23 00:57:19 +0900

    ✨ Add Schedule Repository & dto

--- scapture/src/main/java/com/server/scapture/schedule/repository/ScheduleRepository.java
+++ scapture/src/main/java/com/server/scapture/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,9 @@
 package com.server.scapture.schedule.repository;
 
+import com.server.scapture.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ScheduleRepository {
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
