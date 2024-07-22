39cbd062a191ebaa29d7fa610db365ba2486e98f
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-07-22 10:09:00 +0900

    ✨ 소셜로그인 구헌 - KakaoUserInfo 생성

--- scapture/src/main/java/com/server/scapture/user/dto/KakaoUserInfo.java
+++ scapture/src/main/java/com/server/scapture/user/dto/KakaoUserInfo.java
@@ -5,22 +5,23 @@ import lombok.AllArgsConstructor;
 import java.util.Map;
 
 @AllArgsConstructor
-public class GoogleUserInfo implements OAuth2UserInfo {
-
+public class KakaoUserInfo implements OAuth2UserInfo{
     private Map<String, Object> attributes;
 
     @Override
     public String getProviderId() {
-        return (String) attributes.get("sub");
+        // Long 타입이기 때문에 toString으로 변환
+        return attributes.get("id").toString();
     }
 
     @Override
     public String getProvider() {
-        return "google";
+        return "kakao";
     }
 
     @Override
     public String getName() {
-        return (String) attributes.get("name");
+        // kakao_account라는 Map에서 추출
+        return (String) ((Map) attributes.get("properties")).get("nickname");
     }
 }
