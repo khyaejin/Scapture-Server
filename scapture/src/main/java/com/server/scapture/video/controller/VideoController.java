68326eed152f8238655a40fdcdce4abf5a5e8388
이현승 <77336664+gesal03@users.noreply.github.com>
2024-07-23 22:37:07 +0900

    🩹 Modify URI

--- scapture/src/main/java/com/server/scapture/video/controller/VideoController.java
+++ scapture/src/main/java/com/server/scapture/video/controller/VideoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@RequestMapping("/api/video")
+@RequestMapping("/api/videos")
 @RestController
 @RequiredArgsConstructor
 public class VideoController {
