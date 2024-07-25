5b0db6aaa1d840fa90a3dc9e09d785e49b9ab355
이현승 <77336664+gesal03@users.noreply.github.com>
2024-07-25 20:48:13 +0900

    ✨ Add CreateComment Logic

--- scapture/src/main/java/com/server/scapture/comment/controller/CommentController.java
+++ scapture/src/main/java/com/server/scapture/comment/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.server.scapture.comment.controller;
 
 import com.server.scapture.comment.dto.CreateCommentRequestDto;
+import com.server.scapture.comment.service.CommentService;
 import com.server.scapture.util.response.CustomAPIResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("api/comments")
 public class CommentController {
+    private final CommentService commentService;
     @PostMapping
     public ResponseEntity<CustomAPIResponse<?>> createComment(@RequestHeader(HttpHeaders.AUTHORIZATION) String header, @RequestBody CreateCommentRequestDto createCommentRequestDto) {
-        return null;
+        return commentService.createComment(header, createCommentRequestDto);
     }
 }
