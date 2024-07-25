7418abb64b81a60c2e56142eaca13c2505b70e09
이현승 <77336664+gesal03@users.noreply.github.com>
2024-07-25 21:02:50 +0900

    ✨ Add Create Comment Like Controller & Service

--- scapture/src/main/java/com/server/scapture/comment/controller/CommentController.java
+++ scapture/src/main/java/com/server/scapture/comment/controller/CommentController.java
@@ -17,4 +17,8 @@ public class CommentController {
     public ResponseEntity<CustomAPIResponse<?>> createComment(@RequestHeader(HttpHeaders.AUTHORIZATION) String header, @RequestBody CreateCommentRequestDto createCommentRequestDto) {
         return commentService.createComment(header, createCommentRequestDto);
     }
+    @PostMapping("/{commentId}/likes")
+    public ResponseEntity<CustomAPIResponse<?>> createCommentLikse(@RequestHeader(HttpHeaders.AUTHORIZATION) String header, @PathVariable("commentId") Long commentId) {
+        return null;
+    }
 }
--- scapture/src/main/java/com/server/scapture/comment/service/CommentServiceImpl.java
+++ scapture/src/main/java/com/server/scapture/comment/service/CommentServiceImpl.java
@@ -60,4 +60,8 @@ public class CommentServiceImpl implements CommentService{
                 .status(HttpStatus.CREATED)
                 .body(responseBody);
     }
+    @Override
+    public ResponseEntity<CustomAPIResponse<?>> createCommentLike(String header, Long commentId) {
+        return null;
+    }
 }
