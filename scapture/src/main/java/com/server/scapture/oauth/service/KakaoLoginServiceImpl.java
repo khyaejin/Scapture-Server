d8831e48d3c33f6aedd71942b38fa0a6fd823808
김혜진 <gimhyejin@gimhyejin-ui-MacBookAir.local>
2024-08-06 14:08:54 +0900

    ♻️ [Refact] 카카오 소셜로그인 - 접근토큰 받기 수정

--- scapture/src/main/java/com/server/scapture/oauth/service/KakaoLoginServiceImpl.java
+++ scapture/src/main/java/com/server/scapture/oauth/service/KakaoLoginServiceImpl.java
@@ -8,8 +8,11 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -38,134 +41,44 @@ public class KakaoLoginServiceImpl extends AbstractSocialLoginService implements
 
     @Override
     public ResponseEntity<CustomAPIResponse<?>> getAccessToken(String code, String state) {
-        String accessToken;
+        String reqUrl = "https://kauth.kakao.com/oauth/token";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("grant_type", "authorization_code");
+        requestBody.add("client_id", kakaoApiKey);
+        requestBody.add("client_secret", kakaoClientSecret);
+        requestBody.add("redirect_uri", kakaoRedirectUri);
+        requestBody.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, headers);
+
         try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-            conn.setDoOutput(true);
-
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=").append(kakaoApiKey);
-            sb.append("&client_secret=").append(kakaoClientSecret);
-            sb.append("&redirect_uri=").append(kakaoRedirectUri);
-            sb.append("&code=").append(code);
-
-            try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()))) {
-                bw.write(sb.toString());
-                bw.flush();
-            }
-
-            int responseCode = conn.getResponseCode();
-            logger.info("Token Response Code: {}", responseCode);
-
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(
-                    responseCode >= 200 && responseCode < 300 ? conn.getInputStream() : conn.getErrorStream()))) {
-                String line;
-                StringBuilder responseSb = new StringBuilder();
-                while ((line = br.readLine()) != null) {
-                    responseSb.append(line);
-                }
-                String result = responseSb.toString();
-                logger.info("Token Response Body: {}", result);
-
-                JSONObject jsonObject = new JSONObject(result);
+            ResponseEntity<String> response = restTemplate.exchange(reqUrl, HttpMethod.POST, request, String.class);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                JSONObject jsonObject = new JSONObject(response.getBody());
                 if (jsonObject.has("access_token")) {
-                    accessToken = jsonObject.getString("access_token");
+                    String accessToken = jsonObject.getString("access_token");
+                    CustomAPIResponse<?> res = CustomAPIResponse.createSuccess(200, accessToken, "접근 토큰을 성공적으로 받았습니다.");
+                    return ResponseEntity.status(200).body(res);
                 } else {
-                    CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(401,"이미 사용되었거나 유효하지 않은 인가 코드 입니다.");
+                    CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(401, "이미 사용되었거나 유효하지 않은 인가 코드 입니다.");
                     return ResponseEntity.status(401).body(res);
                 }
+            } else {
+                CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(response.getStatusCodeValue(), "접근 토큰을 받는데 실패했습니다.");
+                return ResponseEntity.status(response.getStatusCodeValue()).body(res);
             }
         } catch (Exception e) {
             logger.error("Error getting access token", e);
-            CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(401,"접근 토큰을 받는데 실패했습니다.");
-            return ResponseEntity.status(401).body(res);
+            CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(500, "접근 토큰을 받는데 실패했습니다.");
+            return ResponseEntity.status(500).body(res);
         }
-
-        CustomAPIResponse<?> res = CustomAPIResponse.createSuccess(200, accessToken, "접근 토큰을 성공적으로 받았습니다.");
-        return ResponseEntity.status(200).body(res);
     }
 
-    @Override
-    public ResponseEntity<CustomAPIResponse<?>> getUserInfo(String accessToken) {
-        String providerId = null;
-        String provider = "kakao";
-        String nickname = null;
-        String email = null;
-        String profileImageUrl = null;
 
-        String reqUrl = "https://kapi.kakao.com/v2/user/me";
-        try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-            conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-            int responseCode = conn.getResponseCode();
-            logger.info("Response Code: {}", responseCode);
-
-            if (responseCode == 401) {
-                CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(401, "토큰이 만료되었거나 유효하지 않은 토큰입니다.");
-                return ResponseEntity.status(401).body(res);
-            }
-
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(
-                    responseCode >= 200 && responseCode <= 300 ? conn.getInputStream() : conn.getErrorStream()))) {
-                String line;
-                StringBuilder responseSb = new StringBuilder();
-                while ((line = br.readLine()) != null) {
-                    responseSb.append(line);
-                }
-                String result = responseSb.toString();
-                logger.info("User Info Response Body: {}", result);
-
-                JSONObject jsonObject = new JSONObject(result);
-
-                if (jsonObject.has("id")) {
-                    providerId = String.valueOf(jsonObject.getLong("id"));
-                } else {
-                    logger.warn("No 'id' field in response");
-                }
-
-                if (jsonObject.has("properties")) {
-                    nickname = jsonObject.getJSONObject("properties").optString("nickname", null);
-                } else {
-                    logger.warn("No 'properties' field in response");
-                }
-
-                if (jsonObject.has("kakao_account")) {
-                    JSONObject kakaoAccount = jsonObject.getJSONObject("kakao_account");
-                    email = kakaoAccount.optString("email", null);
-
-                    if (kakaoAccount.has("profile")) {
-                        JSONObject profile = kakaoAccount.getJSONObject("profile");
-                        profileImageUrl = profile.optString("profile_image_url", null);
-                    } else {
-                        logger.warn("No 'profile' field in 'kakao_account'");
-                    }
-                } else {
-                    logger.warn("No 'kakao_account' field in response");
-                }
-            }
-        } catch (Exception e) {
-            logger.error("Error getting user info", e);
-            CustomAPIResponse<?> res = CustomAPIResponse.createFailWithoutData(400, "유저 정보를 가져오는데 실패했습니다.");
-            return ResponseEntity.status(400).body(res);
-        }
-
-        UserInfo userInfo = UserInfo.builder()
-                .provider(provider)
-                .providerId(providerId)
-                .name(nickname)
-                .email(email)
-                .image(profileImageUrl)
-                .build();
-        CustomAPIResponse<?> res = CustomAPIResponse.createSuccess(200, userInfo, "유저 정보를 성공적으로 가져왔습니다.");
-        return ResponseEntity.status(200).body(res);
-    }
     //login()은 모든 소셜 로그인에서 공통됨 -> 추상 클래스로 구현
 }
